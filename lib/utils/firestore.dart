f54324ce408e6160472826d059b35d7bec6eff1b
John <XuJohn2004@gmail.com>
2020-07-15 15:24:14 -0700

    Firestore methods
    Co-authored-by: Justin Sun <justinsunyt@gmail.com>

--- lib/utils/firestore.dart
+++ lib/utils/firestore.dart
@@ -1,1 +1,25 @@
-class FirestoreProvider {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:Focal/constants.dart';
+import 'package:flutter/cupertino.dart';
+
+class FirestoreProvider {
+  // create user document in firestore when signed in with google
+  static void createUserDocument(FirebaseUser user) {
+    db.collection('users').document(user.uid).setData({
+      'name': user.displayName,
+      'email': user.email,
+    });
+  }
+
+  // get the list of tasks in splash screen
+  List<Widget> getTask() {}
+
+  // add task to firestore method
+  static void addTask() {}
+
+  // update method
+  static void updateTask() {}
+
+  // delete task
+  static void deleteTask() {}
+}
