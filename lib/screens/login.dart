6b51ab899d66c0643a241ac542e1689add1da376
Justin Sun <justinsunyt@gmail.com>
2020-07-22 10:30:29 -0700

    Login animation
    Co-authored-by: John-Xu1 <XuJohn400@gmail.com>

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -7,7 +7,6 @@ import 'package:provider/provider.dart';
 import '../components/wrapper.dart';
 import '../components/rct_button.dart';
 import 'package:Focal/constants.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:Focal/utils/firestore.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
@@ -19,20 +18,35 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool _isLoading = false;
+  bool _loginLoading = false;
+  bool _isLoading = true;
+  bool _isLogin = false;
   @override
   void initState() {
     super.initState();
     auth.onAuthStateChanged.listen((user) {
       Provider.of<User>(context, listen: false).user = user;
       if (user == null) {
-        Navigator.popUntil(context, (route) => route.isFirst);
+        setState(() {
+          _isLogin = true;
+          _isLoading = false;
+        });
+        Future.delayed(Duration(milliseconds: 1000), () {
+          Navigator.popUntil(context, (route) => route.isFirst);
+        });
       } else {
         FirestoreProvider(user).userDocumentExists().then((exists) {
+          setState(() {
+            _isLoading = false;
+          });
           if (exists) {
-            Navigator.pushNamed(context, '/home');
+            Future.delayed(Duration(milliseconds: 1000), () {
+              Navigator.pushNamed(context, '/home');
+            });
           } else {
-            Navigator.pushNamed(context, '/onboarding');
+            Future.delayed(Duration(milliseconds: 1000), () {
+              Navigator.pushNamed(context, '/onboarding');
+            });
           }
         });
       }
@@ -41,42 +55,64 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: _isLoading,
-      child: WrapperWidget(
-        nav: false,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(image: AssetImage('images/Focal Logo_Full.png')),
-            Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: RctButton(
-                onTap: () async {
-                  setState(() {
-                    _isLoading = true;
-                  });
-                  AuthProvider().googleSignIn();
-                  setState(() {
-                    _isLoading = false;
-                  });
-                  LocalNotificationHelper.userLoggedIn = true;
-                },
-                buttonWidth: 315,
-                buttonColor: Colors.black,
-                textColor: Colors.white,
-                buttonText: "Sign in with Google",
-                textSize: 24,
-                icon: FaIcon(
-                  FontAwesomeIcons.google,
-                  color: Colors.white,
-                  size: 30,
-                ),
+    return Stack(
+      children: <Widget>[
+        Container(
+          color: Colors.white,
+        ),
+        Visibility(
+          visible: _isLogin,
+          child: ModalProgressHUD(
+            inAsyncCall: _loginLoading,
+            child: WrapperWidget(
+              nav: false,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image(image: AssetImage('images/Focal Logo_Full.png')),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100),
+                    child: RctButton(
+                      onTap: () async {
+                        setState(() {
+                          _loginLoading = true;
+                        });
+                        AuthProvider().googleSignIn();
+                        setState(() {
+                          _loginLoading = false;
+                        });
+                        LocalNotificationHelper.userLoggedIn = true;
+                      },
+                      buttonWidth: 315,
+                      buttonColor: Colors.black,
+                      textColor: Colors.white,
+                      buttonText: "Sign in with Google",
+                      textSize: 24,
+                      icon: FaIcon(
+                        FontAwesomeIcons.google,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
-      ),
+        Visibility(
+          visible: !_isLogin,
+          child: AnimatedOpacity(
+            opacity: _isLoading ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 1000),
+            child: Container(
+              alignment: Alignment.center,
+              color: Colors.white,
+              child: Image(image: AssetImage('images/Focal Logo_Full.png')),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
