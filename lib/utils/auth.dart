95e7bc527fa3b124b7e2a0676c45d35d014c0ab1
michaelq629 <michael.que@icloud.com>
2020-07-24 10:48:18 -0700

    Sign in with Apple Auth
    Part in xcode should be done for sure. I'm missing the rest of the code on the UI Part
    https://codewithandrea.com/videos/2020-01-20-apple-sign-in-flutter-firebase/

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,9 +1,53 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:apple_sign_in/apple_sign_in.dart';
+// import 'package:apple_sign_in_firebase_flutter/apple_sign_in_available.dart';
+// import 'package:apple_sign_in_firebase_flutter/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appleSignInAvailable =
+        Provider.of<AppleSignInAvailable>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [            
+            if (appleSignInAvailable.isAvailable)
+              AppleSignInButton(
+                style: ButtonStyle.black, // style as needed
+                type: ButtonType.signIn, // style as needed
+                onPressed: () {},
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppleSignInAvailable {
+  AppleSignInAvailable(this.isAvailable);
+  final bool isAvailable;
+
+  static Future<AppleSignInAvailable> check() async {
+    return AppleSignInAvailable(await AppleSignIn.isAvailable());
+  }
+}
 
 class AuthProvider {
   FirebaseAuth _auth = FirebaseAuth.instance;
   GoogleSignIn _googleSignIn = GoogleSignIn();
+
+
+  
 
   Future<FirebaseUser> getUser() async => _auth.currentUser();
 
