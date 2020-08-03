41a83648667eb8345fc63199bdacec80031b43df
Justin Sun <justin@justins-mbp.lan>
2020-08-03 11:27:50 -0700

    Resized TaskStatTile

--- lib/components/task_stat_tile.dart
+++ lib/components/task_stat_tile.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'task_item.dart';
 import 'package:Focal/constants.dart';
+import 'package:Focal/utils/size_config.dart';
 
 class TaskStatTile extends StatelessWidget {
   final int maxTime;
   final TaskItem task;
   final bool completed;
-  const TaskStatTile({@required this.maxTime, @required this.task, @required this.completed});
+  const TaskStatTile(
+      {@required this.maxTime, @required this.task, @required this.completed});
 
   @override
   Widget build(BuildContext context) {
     double height = 10;
-    double maxLength = 250;
+    double maxLength = SizeConfig.safeBlockHorizontal * 100 - 180;
 
     return SizedBox(
       height: 38,
@@ -32,13 +34,9 @@ class TaskStatTile extends StatelessWidget {
             bottom: 0,
             right: 0,
             child: Text(
-              (task.secondsFocused ~/ 60)
-                      .toString()
-                      .padLeft(2, "0") +
+              (task.secondsFocused ~/ 60).toString().padLeft(2, "0") +
                   ":" +
-                  (task.secondsFocused % 60)
-                      .toString()
-                      .padLeft(2, "0"),
+                  (task.secondsFocused % 60).toString().padLeft(2, "0"),
               style: TextStyle(
                 fontSize: 12,
                 fontWeight: FontWeight.w500,
@@ -53,7 +51,13 @@ class TaskStatTile extends StatelessWidget {
               children: <Widget>[
                 Container(
                   height: height,
-                  width: maxTime == 0 ? 0 : (task.secondsPaused + task.secondsDistracted + task.secondsFocused) / maxTime * maxLength,
+                  width: maxTime == 0
+                      ? 0
+                      : (task.secondsPaused +
+                              task.secondsDistracted +
+                              task.secondsFocused) /
+                          maxTime *
+                          maxLength,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(5)),
                     color: Theme.of(context).dividerColor,
@@ -61,7 +65,11 @@ class TaskStatTile extends StatelessWidget {
                 ),
                 Container(
                   height: height,
-                  width: maxTime == 0 ? 0 : (task.secondsDistracted + task.secondsFocused) / maxTime * maxLength,
+                  width: maxTime == 0
+                      ? 0
+                      : (task.secondsDistracted + task.secondsFocused) /
+                          maxTime *
+                          maxLength,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(5)),
                     color: Colors.red,
@@ -69,7 +77,9 @@ class TaskStatTile extends StatelessWidget {
                 ),
                 Container(
                   height: height,
-                  width: maxTime == 0 ? 0 : task.secondsFocused / maxTime * maxLength,
+                  width: maxTime == 0
+                      ? 0
+                      : task.secondsFocused / maxTime * maxLength,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(5)),
                     color: Theme.of(context).primaryColor,
