a4e7b54332ef8d6b5fcce88efa8dc662a509364e
Justin Sun <justin@justins-mbp.lan>
2020-08-11 13:31:13 -0700

    Dev and Prod flavors for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,51 +1,33 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    
-    override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         var screenStat = true
         
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let CHANNEL = FlutterMethodChannel(name: "com.flutter.lockscreen", binaryMessenger: controller as! FlutterBinaryMessenger)
         
         CHANNEL.setMethodCallHandler {
-          
-            
             [unowned self] (methodCall, result) in
-            if (methodCall.method == "printBoi")
-            {
+            if (methodCall.method == "printBoi") {
                 if UIScreen.main.brightness == 0.0 {
-                           screenStat = false
-                    
-                }
-                else {
+                    screenStat = false
+                } else {
                     screenStat = true
                     if #available(iOS 10.0, *) {
-                                       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-                                   } else {
-                                       // Fallback on earlier versions
-                                   }
-                    
+                        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+                    } else {
+                        // Fallback on earlier versions
+                    }
                 }
                 result(screenStat)
             }
         }
-        
-        
-        
-        
-        
+        FirebaseApp.configure()
         GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-            
-
-       
-
-}
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)      
+    }
 }
