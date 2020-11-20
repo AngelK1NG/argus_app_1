d46cac984acbff4fc03540711ed91405b0684618
Justin Sun <justin@justins-mbp.lan>
2020-11-20 00:05:10 -0800

    oops

--- lib/constants.dart
+++ lib/constants.dart
@@ -51,7 +51,7 @@ num voltsIncrement({
 }) {
   num increment = 0;
   increment = 0.002 *
-      (secondsFocused * 600 - secondsDistracted) *
+      (secondsFocused - secondsDistracted) *
       pow(100 * (completedTasks + 1) / totalTasks, 0.1) *
       pow(totalTasks, 0.1) /
       pow(numPaused + 1, 0.2);
@@ -72,7 +72,7 @@ num voltsDecay({
   num decay = 0;
   if (startedTasks != 0 && completedTasks != totalTasks) {
     decay = 0.3 *
-            pow(seconds * 600, 0.3) *
+            pow(seconds, 0.3) *
             pow(100 * startedTasks / totalTasks, 0.1) *
             pow(totalTasks, 0.1) +
         pow((seconds % 4) + 1, 0.03) * pow((seconds % 5) + 1, 0.03) -
