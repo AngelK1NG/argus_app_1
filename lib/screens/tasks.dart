d493af7017396bd4a2c42f448d333d6494a60d52
Justin Sun <justinsunyt@gmail.com>
2021-01-20 01:17:03 -0800

    Task delete; Fixed?: cannot delete/reorder when spamming probably not working LOL

--- lib/screens/tasks.dart
+++ lib/screens/tasks.dart
@@ -70,7 +70,20 @@ class _TasksPageState extends State<TasksPage> {
       taskMap.forEach((date, tasks) {
         List<DragAndDropItem> newTasks = [];
         for (var task in tasks) {
-          newTasks.add(DragAndDropItem(child: task, canDrag: !task.completed));
+          Task newTask;
+          newTask = Task(
+            index: task.index,
+            name: task.name,
+            date: task.date,
+            completed: task.completed,
+            paused: task.paused,
+            seconds: task.seconds,
+            id: task.id,
+            onDismissed: (direction) => onDismissed(direction, newTask),
+            onTap: () {},
+          );
+          newTasks
+              .add(DragAndDropItem(child: newTask, canDrag: !task.completed));
         }
         newLists.add(DragAndDropList(
           canDrag: false,
@@ -101,6 +114,72 @@ class _TasksPageState extends State<TasksPage> {
     _date = DateProvider().today;
   }
 
+  void onDismissed(DismissDirection direction, Task task) {
+    if (!_saving) {
+      if (direction == DismissDirection.startToEnd) {
+      } else {
+        setState(() => _saving = true);
+        task.deleteDoc(Provider.of<UserStatus>(context, listen: false));
+        List<DragAndDropItem> newTasks = [];
+        int listIndex;
+        String listDate;
+        if (task.completed) {
+          listIndex = _tasks.length - 1;
+        } else {
+          for (var i = 0; i < _tasks.length; i++) {
+            DragAndDropList list = _tasks[i];
+            TaskListHeader header = list.header;
+            if (task.date.isEmpty
+                ? header.date == 'No Date'
+                : header.date == task.date) {
+              listIndex = i;
+              listDate = header.date;
+            }
+          }
+        }
+        _tasks[listIndex].children.removeAt(task.index);
+        for (var i = 0; i < _tasks[listIndex].children.length; i++) {
+          Task task = _tasks[listIndex].children[i].child;
+          Task newTask;
+          newTask = Task(
+            id: task.id,
+            index: i,
+            name: task.name,
+            date: task.date,
+            completed: task.completed,
+            paused: task.paused,
+            seconds: task.seconds,
+            onDismissed: (direction) => onDismissed(direction, newTask),
+          );
+          newTasks.add(DragAndDropItem(child: newTask));
+        }
+        DragAndDropList removedList = _tasks.removeAt(listIndex);
+        if (removedList.children.isNotEmpty) {
+          _tasks.insert(
+            listIndex,
+            DragAndDropList(
+              canDrag: false,
+              header: TaskListHeader(date: listDate),
+              children: newTasks,
+            ),
+          );
+          int updatedTasks = 0;
+          newTasks.forEach((item) {
+            Task task = item.child;
+            task.updateDoc(Provider.of<UserStatus>(context, listen: false), () {
+              updatedTasks++;
+              if (updatedTasks == newTasks.length) {
+                setState(() => _saving = false);
+              }
+            });
+          });
+        } else {
+          setState(() => _saving = false);
+        }
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -127,135 +206,246 @@ class _TasksPageState extends State<TasksPage> {
           right: 0,
           left: 0,
           top: 65,
-          child: SizedBox(
-            height: SizeProvider.safeHeight - 80,
-            child: DragAndDropLists(
-              itemDecorationWhileDragging: BoxDecoration(
-                color: white.withOpacity(0.8),
-                boxShadow: [
-                  BoxShadow(
-                    color: blue.withOpacity(0.2),
-                    spreadRadius: 0,
-                    blurRadius: 20, // changes position of shadow
-                  ),
-                ],
-              ),
-              itemGhostOpacity: 1,
-              itemGhost: Divider(
-                color: Theme.of(context).primaryColor,
-                thickness: 2,
-                height: 0,
-              ),
-              listGhostOpacity: 0,
-              lastListTargetSize: 0,
-              lastItemTargetHeight: 15,
-              addLastItemTargetHeightToTop: true,
-              itemOnWillAccept: (_, target) {
-                Task task = target.child;
-                if ((task.date.isNotEmpty &&
-                        DateTime.parse(task.date)
-                            .isBefore(DateProvider().today)) ||
-                    task.completed) {
-                  return false;
-                } else {
-                  return true;
-                }
-              },
-              itemTargetOnWillAccept: (_, target) {
-                DragAndDropList list = target.parent;
-                TaskListHeader header = list.header;
-                if (header.date == 'Overdue' || header.date == 'Completed') {
-                  return false;
-                } else {
-                  return true;
-                }
-              },
-              onItemReorder: (int oldItemIndex, int oldListIndex,
-                  int newItemIndex, int newListIndex) {
-                TaskListHeader header = _tasks[newListIndex].header;
-                Task movedTask =
-                    _tasks[oldListIndex].children.removeAt(oldItemIndex).child;
-                Task newMovedTask = Task(
-                  id: movedTask.id,
-                  index: newItemIndex,
-                  name: movedTask.name,
-                  date: header.date == 'No Date' ? '' : header.date,
-                  completed: movedTask.completed,
-                  paused: movedTask.paused,
-                  seconds: movedTask.seconds,
-                );
-                _saving = true;
-                setState(() {
-                  _tasks[newListIndex].children.insert(
-                        newItemIndex,
-                        DragAndDropItem(child: newMovedTask),
-                      );
-                });
-                List newTasks = [];
-                for (var i = 0; i < _tasks[oldListIndex].children.length; i++) {
-                  Task task = _tasks[oldListIndex].children[i].child;
-                  Task newTask = Task(
-                    id: task.id,
-                    index: i,
-                    name: task.name,
-                    date: task.date,
-                    completed: task.completed,
-                    paused: task.paused,
-                    seconds: task.seconds,
-                  );
-                  newTasks.add(newTask);
-                }
-                for (var i = 0; i < _tasks[newListIndex].children.length; i++) {
-                  Task task = _tasks[newListIndex].children[i].child;
-                  Task newTask = Task(
-                    id: task.id,
-                    index: i,
-                    name: task.name,
-                    date: task.date,
-                    completed: task.completed,
-                    paused: task.paused,
-                    seconds: task.seconds,
-                  );
-                  newTasks.add(newTask);
-                }
-                if (_tasks[oldListIndex].children.isEmpty) {
-                  _tasks.removeAt(oldListIndex);
-                }
-                int updatedTasks = 0;
-                newTasks.forEach((task) {
-                  task.updateDoc(user, () {
-                    updatedTasks++;
-                    if (updatedTasks == newTasks.length) {
-                      _saving = false;
-                    }
-                  });
-                });
-              },
-              contentsWhenEmpty: Center(
-                child: Column(
-                  children: [
-                    Text(
-                      'Start fresh.',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20, left: 50, right: 50),
-                      child: Text(
-                        'Got something to do? Add it by tapping the + button.',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
+          child: AbsorbPointer(
+            absorbing: _saving,
+            child: SizedBox(
+              height: SizeProvider.safeHeight - 80,
+              child: DragAndDropLists(
+                itemDecorationWhileDragging: BoxDecoration(
+                  color: white.withOpacity(0.8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: blue.withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 20, // changes position of shadow
                     ),
                   ],
                 ),
+                itemGhostOpacity: 1,
+                itemGhost: Divider(
+                  color: Theme.of(context).primaryColor,
+                  thickness: 2,
+                  height: 0,
+                ),
+                listGhostOpacity: 0,
+                lastListTargetSize: 0,
+                lastItemTargetHeight: 15,
+                addLastItemTargetHeightToTop: true,
+                itemOnWillAccept: (_, target) {
+                  Task task = target.child;
+                  if ((task.date.isNotEmpty &&
+                          DateTime.parse(task.date)
+                              .isBefore(DateProvider().today)) ||
+                      task.completed) {
+                    return false;
+                  } else {
+                    return true;
+                  }
+                },
+                itemTargetOnWillAccept: (_, target) {
+                  DragAndDropList list = target.parent;
+                  TaskListHeader header = list.header;
+                  if (header.date == 'Overdue' || header.date == 'Completed') {
+                    return false;
+                  } else {
+                    return true;
+                  }
+                },
+                onItemReorder: (int oldItemIndex, int oldListIndex,
+                    int newItemIndex, int newListIndex) {
+                  TaskListHeader oldListHeader = _tasks[oldListIndex].header;
+                  TaskListHeader newListHeader = _tasks[newListIndex].header;
+                  Task movedTask = _tasks[oldListIndex]
+                      .children
+                      .removeAt(oldItemIndex)
+                      .child;
+                  Task newMovedTask;
+                  newMovedTask = Task(
+                    id: movedTask.id,
+                    index: newItemIndex,
+                    name: movedTask.name,
+                    date: newListHeader.date == 'No Date'
+                        ? ''
+                        : newListHeader.date,
+                    completed: movedTask.completed,
+                    paused: movedTask.paused,
+                    seconds: movedTask.seconds,
+                    onDismissed: (direction) =>
+                        onDismissed(direction, newMovedTask),
+                  );
+                  _saving = true;
+                  setState(() {
+                    _tasks[newListIndex].children.insert(
+                          newItemIndex,
+                          DragAndDropItem(child: newMovedTask),
+                        );
+                  });
+                  List<DragAndDropItem> oldListTasks = [];
+                  List<DragAndDropItem> newListTasks = [];
+                  for (var i = 0;
+                      i < _tasks[oldListIndex].children.length;
+                      i++) {
+                    Task task = _tasks[oldListIndex].children[i].child;
+                    Task newTask;
+                    newTask = Task(
+                      id: task.id,
+                      index: i,
+                      name: task.name,
+                      date: task.date,
+                      completed: task.completed,
+                      paused: task.paused,
+                      seconds: task.seconds,
+                      onDismissed: (direction) =>
+                          onDismissed(direction, newTask),
+                    );
+                    oldListTasks.add(DragAndDropItem(child: newTask));
+                  }
+                  for (var i = 0;
+                      i < _tasks[newListIndex].children.length;
+                      i++) {
+                    Task task = _tasks[newListIndex].children[i].child;
+                    Task newTask;
+                    newTask = Task(
+                      id: task.id,
+                      index: i,
+                      name: task.name,
+                      date: task.date,
+                      completed: task.completed,
+                      paused: task.paused,
+                      seconds: task.seconds,
+                      onDismissed: (direction) =>
+                          onDismissed(direction, newTask),
+                    );
+                    newListTasks.add(DragAndDropItem(child: newTask));
+                  }
+                  DragAndDropList removedOldList =
+                      _tasks.removeAt(oldListIndex);
+                  int updatedTasks = 0;
+                  if (oldListIndex == newListIndex) {
+                    _tasks.insert(
+                      oldListIndex,
+                      DragAndDropList(
+                        canDrag: false,
+                        header: TaskListHeader(date: oldListHeader.date),
+                        children: oldListTasks,
+                      ),
+                    );
+                    oldListTasks.forEach((item) {
+                      Task task = item.child;
+                      task.updateDoc(user, () {
+                        updatedTasks++;
+                        if (updatedTasks == oldListTasks.length) {
+                          setState(() => _saving = false);
+                        }
+                      });
+                    });
+                  } else {
+                    if (oldListIndex > newListIndex) {
+                      _tasks.removeAt(newListIndex);
+                      _tasks.insert(
+                        newListIndex,
+                        DragAndDropList(
+                          canDrag: false,
+                          header: TaskListHeader(date: newListHeader.date),
+                          children: newListTasks,
+                        ),
+                      );
+                    } else {
+                      _tasks.removeAt(newListIndex - 1);
+                      _tasks.insert(
+                        newListIndex - 1,
+                        DragAndDropList(
+                          canDrag: false,
+                          header: TaskListHeader(date: newListHeader.date),
+                          children: newListTasks,
+                        ),
+                      );
+                    }
+                    if (removedOldList.children.isNotEmpty) {
+                      _tasks.insert(
+                        oldListIndex,
+                        DragAndDropList(
+                          canDrag: false,
+                          header: TaskListHeader(date: oldListHeader.date),
+                          children: oldListTasks,
+                        ),
+                      );
+                      oldListTasks.forEach((item) {
+                        Task task = item.child;
+                        task.updateDoc(user, () {
+                          updatedTasks++;
+                          if (updatedTasks ==
+                              oldListTasks.length + newListTasks.length) {
+                            setState(() => _saving = false);
+                          }
+                        });
+                      });
+                      newListTasks.forEach((item) {
+                        Task task = item.child;
+                        task.updateDoc(user, () {
+                          updatedTasks++;
+                          if (updatedTasks ==
+                              oldListTasks.length + newListTasks.length) {
+                            setState(() => _saving = false);
+                          }
+                        });
+                      });
+                    } else {
+                      newListTasks.forEach((item) {
+                        Task task = item.child;
+                        task.updateDoc(user, () {
+                          updatedTasks++;
+                          if (updatedTasks == newListTasks.length) {
+                            setState(() => _saving = false);
+                          }
+                        });
+                      });
+                    }
+                  }
+                },
+                contentsWhenEmpty: Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Start fresh.',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20, left: 50, right: 50),
+                        child: Text(
+                          'Got something to do? Add it by tapping the + button.',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                children: _tasks,
               ),
-              children: _tasks,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 10,
+          bottom: 10,
+          child: AnimatedOpacity(
+            opacity: _saving ? 1 : 0,
+            duration: fadeDuration,
+            curve: fadeCurve,
+            child: SizedBox(
+              height: 60,
+              width: 60,
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.transparent,
+                strokeWidth: 2,
+              ),
             ),
           ),
         ),
@@ -264,19 +454,21 @@ class _TasksPageState extends State<TasksPage> {
           bottom: 15,
           child: Button(
             onTap: () {
-              Navigator.of(context).push(PageRouteBuilder(
-                opaque: false,
-                transitionDuration: Duration(seconds: 5),
-                pageBuilder: (_, __, ___) {
-                  return AddOverlay(
-                    text: _text,
-                    setText: (text) => _text = text,
-                    date: _date,
-                    setDate: (date) => _date = date,
-                    submit: submitTask,
-                  );
-                },
-              ));
+              if (!_saving) {
+                Navigator.of(context).push(PageRouteBuilder(
+                  opaque: false,
+                  transitionDuration: Duration(seconds: 5),
+                  pageBuilder: (_, __, ___) {
+                    return AddOverlay(
+                      text: _text,
+                      setText: (text) => _text = text,
+                      date: _date,
+                      setDate: (date) => _date = date,
+                      submit: submitTask,
+                    );
+                  },
+                ));
+              }
             },
             width: 50,
             row: Row(
