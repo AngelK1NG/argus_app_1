e21da5550ecb1e8ab1475401c7a51188298079f3
unknown <ancervantes@s.sfusd.edu>
2021-07-25 08:15:07 -0700

    updated verssion 7/24

--- vivi.ino
+++ vivi.ino
@@ -1,17 +1,19 @@
+ //#include <ESP8266Firebase.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ezTime.h>
 
+//#define Project_ID "vivi11"
 Timezone myLocalTime; 
 
 const char* ssid = "boku no"; //SSID of wifi network
 const char* password = "pico";// Password for wifi network
-const int alarmHour= 6; // just while we don't have firebase or app to set it
-const int alarmMinute= 9; // ^^^^^ (sets minute for alarm)
-const int buzzerPin = 0; // pin of buzzer (D3 on NodeMCU)
+const int alarmHour= 19; // just while we don't have firebase or app to set it
+const int alarmMinute= 7; // ^^^^^ (sets minute for alarm)
+const int buzzerPin = 14; // pin of buzzer (D5 on NodeMCU)
 
 
-
+//Firebase milky(Project_ID);
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,24 +30,36 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-
+Serial.println("connected");
   // Set desired time zone for Timezone object declared in the beginning
   myLocalTime.setLocation(F("America/Los_Angeles")); // set your time zone
 
   // Sync NTP time for ezTime library
   waitForSync(); 
-
+Serial.println("synced");
   delay(2000);
+
+//  milky.setInt("Hour", 4 ); // just while we get the app to write times into the database
+//  milky.setInt("Minute", 32); //^^^^
 }
 
 void loop() {
   int currentHour = myLocalTime.hour();
   int currentMinute = myLocalTime.minute();
-Serial.println(myLocalTime.minute());
+Serial.print(myLocalTime.hour());
+Serial.print(":");
+Serial.print(myLocalTime.minute());
+Serial.println();
+
 if (currentHour == alarmHour && currentMinute == alarmMinute) {
-  digitalWrite( buzzerPin, HIGH);
+  digitalWrite(buzzerPin, HIGH);
   delay(2000);
-  digitalWrite( buzzerPin, LOW);
+  digitalWrite(buzzerPin, LOW);
 }
+//if (currentHour == milky.getInt("Hour") && currentMinute == milky.getInt("Minute")) {
+//  digitalWrite( buzzerPin, HIGH);
+//  delay(2000);
+//  digitalWrite(buzzerPin, LOW);
+//}
 delay(1000);
 }
